3916c2854a1f19eed840e3bcba2a0ba996881ced
Ashila Chowdary <kosarajuashila@gmail.com>
2026-03-06 21:18:38 +0530

    Student details Program

---
+++ student details/student.java
@@ -0,0 +1,11 @@
+class Student {
+    public static void main(String[] args) {
+        String name = "Ashila";
+        int age = 22;
+        String course = "CSE";
+
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+    }
+}
